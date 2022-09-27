0ff1b24e78d0f6ec6ec8e034ab13c216e0386d82
ttffkk <tfkaminski@gmail.com>
2022-09-27 16:39:10 +0200

    added Prime Numbers

--- src/App.java
+++ src/App.java
@@ -1,5 +1,20 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println(Math.PI);
+        System.out.println(nextPrime(1190));
     }
+    public static int nextPrime(int n) {
+        for (int i = n + 1; true; i++) {
+            if (isPrime(i)) {
+                return i;
+            }
+        }
+    }
+    public static boolean isPrime(int n) {
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }   
 }
