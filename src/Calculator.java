852742c3d2ff820af00a65de3eb06fa0b7541f45
ttffkk <tfkaminski@gmail.com>
2022-09-30 21:40:13 +0200

    Safety commit

--- src/Calculator.java
+++ src/Calculator.java
@@ -6,5 +6,7 @@ public class Calculator {
     public int subtract(int x, int y){return x-y;}
     public double subtract(double x, double y){return x-y;}
     public long subtract(long x, long y){return x-y;}
-    //public int multiply(int x, int y){if(x*y)
+    public int multiply(int x, int y){return x*y;}
+    public double multiply(double x,double y){return x*y;}
+    public long multiply(long x, long y){x*y;}
 }
