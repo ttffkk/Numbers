763adcb18008a675f1f4a6bc92596ff6fe13ecee
ttffkk <tfkaminski@gmail.com>
2022-09-30 19:24:12 +0200

    Added Calculator

---
+++ src/Calculator.java
@@ -0,0 +1,10 @@
+public class Calculator {
+    public Calculator(){}
+    public int add(int x, int y){return x+y;}
+    public double add(double x, double y){return Double.sum(x, y) ;}
+    public long add(long x, long y){return Long.sum(x, y);}
+    public int subtract(int x, int y){return x-y;}
+    public double subtract(double x, double y){return x-y;}
+    public long subtract(long x, long y){return x-y;}
+    //public int multiply(int x, int y){if(x*y)
+}
